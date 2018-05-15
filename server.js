53472d8daba77a3c77048de10cac892c7020150a
Jai Goyal <goyaljai.y14@gmail.com>
2018-05-15 21:25:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('LNMIIT BLOG!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('on port 3000!'))
